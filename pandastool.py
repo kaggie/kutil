8c81c2e7fc3da57021613c3dd3de4337eb01238a
Josh <kaggie@gmail.com>
2018-10-09 17:46:02 +0100

    .

--- pandastool.py
+++ pandastool.py
@@ -1,17 +1,20 @@
 import pandas
 
 class prep():
-    def __init__(self,datapath = ''):
-        self.data = pandas.read_excel(datapath)
+    def __init__(self,data):        
         self.keys = data.keys()
         self.data = data
         self.uniques = {}
+        self._temp_data = data
         for key in self.keys:
             uniquevals = np.unique(self.data[key])
             #uniquevals = uniquevals[not np.isnan(uniquevals)]
             self.uniques[key] = uniquevals
-    def get_type(self,keyvals = {}):
-        newdata = self.data
+    def get_type(self,keyvals = {}, data = None):
+        if data is None:
+            newdata = self.data
+        else:
+            newdata = data
         for key in keyvals.keys():
             newdata = newdata[newdata[key] == keyvals[key]]
         self._temp_data = newdata
@@ -20,7 +23,8 @@ class prep():
         if data is None:
             data = self._temp_data
         means = data['mean']
+        n = len(means)
         if niceprint:
-            print(str(np.mean(means)) + ' +/- '  + str(np.std(means)))
+            print(str(np.mean(means)) + ' +/- '  + str(np.std(means)) + ' , N = ' + str(n))
             return 
         return np.mean(means), np.std(means)
